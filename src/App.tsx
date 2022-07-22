147975d9572b913c45c5728e73142d5da4c044ad
Ariel Barrios <ariel29111978@hotmail.com>
2022-07-22 15:29:45 -0300

    Crea la inteface Sub y el INICIAL_STATE

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const INICIAL_STATE = [
+  {
+    nick: 'dapelu',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=dapelu',
+    descipion: 'Dapelu hace e moredador aveces',
+  },
+  {
+    nick: 'sergio_serrano',
+    subMonths: 7,
+    avatar: 'https://i.pravatar.cc/150?u=sergio_serrano',
+  }
+]
+
+interface Sub {
+  nick: string
+  avatar: string
+  subMonths: number
+  description?: string
+}
+
 function App() {
-  const [subs, setSubs] = useState([
-    {
-      nick: 'dapelu',
-      subMonths: 3,
-      avatar: 'https://i.pravatar.cc/150?u=dapelu',
-      descipion: 'Dapelu hace e moredador aveces',
-    },
-    {
-      nick: 'sergio_serrano',
-      subMonths: 7,
-      avatar: 'https://i.pravatar.cc/150?u=sergio_serrano',
-    }
-  ]);
+  const [subs, setSubs] = useState<Array<Sub>>([]);
+
+  useEffect(()=>{
+    setSubs(INICIAL_STATE)
+  },[])
 
   return (
     <div className='App'>
@@ -25,7 +38,7 @@ function App() {
             <li key={sub.nick}>
               <img src={sub.avatar} alt={`avatar para ${sub.nick}`} />
               <h2>{sub.nick}(<small>{sub.subMonths}</small>)</h2>
-              <p>{sub.descipion?.substring(0,100)}</p>
+              <p>{sub.description?.substring(0,100)}</p>
             </li>
           ))
         }
@@ -35,3 +48,5 @@ function App() {
 }
 
 export default App
+
+
