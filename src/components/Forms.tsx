a33b20f5a41e21bfbeede5c2d4d93f85dba36c3c
Ariel Barrios <ariel29111978@hotmail.com>
2022-07-23 01:13:08 -0300

    Incorpora la función para manejar los cambios en el formulario

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
 const Forms = () => {
+  const [inputValue, setInputValue] = useState({
+    nick: '',
+    subMonths: 0,
+    avatar: '',
+    description: '',
+  })
   const handleSubmit = () => {};
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setInputValue({
+      ...inputValue,
+      [e.target.name]: e.target.value
+    })
+  }
 
   return(
     <form onSubmit={handleSubmit}>
-      <input type='text' name='nick' placeholder='nick' />
-      <input type='text' name='subMonths' placeholder='subMonths' />
-      <input type='text' name='avatar' placeholder='avatar' />
-      <input type='text' name='description' placeholder='description' />
+      <input onChange={handleChange} value={inputValue.nick} type='text' name='nick' placeholder='nick' />
+      <input onChange={handleChange} value={inputValue.subMonths} type='number' name='subMonths' placeholder='subMonths' />
+      <input onChange={handleChange} value={inputValue.avatar} type='text' name='avatar' placeholder='avatar' />
+      <textarea onChange={handleChange} value={inputValue.description} name='description' placeholder='description' />
       <button>Guardar nuevo</button>
     </form>
 
