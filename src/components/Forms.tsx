038069ba41e49fadbe08d38d8f726b2af043f469
Ariel Barrios <ariel29111978@hotmail.com>
2022-07-25 12:48:33 -0300

    Agrega la función 'Borrar formulario'

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -18,23 +18,34 @@ const Forms = ({ onNewSub }: FormProps) => {
   })
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onNewSub(inputValue)
+    onNewSub(inputValue);
+    handleClear();
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setInputValue({
       ...inputValue,
       [e.target.name]: e.target.value
+    })    
+  }
+
+  const handleClear = () => {
+    setInputValue({
+      nick: '',
+      subMonths: 0,
+      avatar: '',
+      description: '',
     })
   }
 
-  return(
+  return (
     <form onSubmit={handleSubmit}>
       <input onChange={handleChange} value={inputValue.nick} type='text' name='nick' placeholder='nick' />
       <input onChange={handleChange} value={inputValue.subMonths} type='number' name='subMonths' placeholder='subMonths' />
       <input onChange={handleChange} value={inputValue.avatar} type='text' name='avatar' placeholder='avatar' />
       <textarea onChange={handleChange} value={inputValue.description} name='description' placeholder='description' />
-      <button>Guardar nuevo</button>
+      <button type='button'>Borrar formulario</button>
+      <button type='submit'>Guardar nuevo</button>
     </form>
 
   )
