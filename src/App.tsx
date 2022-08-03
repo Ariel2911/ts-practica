4fc38e26b04276735ecc5e301c2633b6066d2844
Ariel Barrios <ariel29111978@hotmail.com>
2022-08-03 20:07:14 -0300

    Fin de práctica

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,19 @@ import Forms from './components/Forms';
 import List from './components/List';
 import { Sub } from '../types';
 
-// const INICIAL_STATE = [
-//   {
-//     nick: 'maxi_dev',
-//     subMonths: 3,
-//     avatar: 'https://i.pravatar.cc/150?u=maxi_dev',
-//     descipion: 'Dapelu hace e moredador aveces',
-//   },
-//   {
-//     nick: 'dev_jony',
-//     subMonths: 7,
-//     avatar: 'https://i.pravatar.cc/150?u=dev_jony',
-//   }
-// ]
+const INICIAL_STATE = [
+  {
+    nick: 'maxi_dev',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=maxi_dev',
+    descipion: 'Dapelu hace e moredador aveces',
+  },
+  {
+    nick: 'dev_jony',
+    subMonths: 7,
+    avatar: 'https://i.pravatar.cc/150?u=dev_jony',
+  }
+]
 
 interface AppState {
   subs: Sub[]
@@ -29,7 +29,7 @@ function App() {
   const divRef = useRef<HTMLDivElement>(null);
 
   useEffect(()=>{
-    // setSubs(INICIAL_STATE)
+    setSubs(INICIAL_STATE)
   },[])
 
   const handleNewSub = (newSub: Sub): void => {
@@ -41,6 +41,7 @@ function App() {
     <div className='App' ref={divRef} >
       <h1>Subs</h1>
       <List subs={subs} />
+      New sub: {newSubNumber}
       <Forms onNewSub={handleNewSub} />
     </div>
   )
