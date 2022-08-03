8afa645a66ddac1beb613048152584a67f5333a5
Ariel Barrios <ariel29111978@hotmail.com>
2022-08-03 19:23:40 -0300

    Agraga un useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,44 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Forms from './components/Forms';
 import List from './components/List';
 import { Sub } from '../types';
 
-const INICIAL_STATE = [
-  {
-    nick: 'maxi_dev',
-    subMonths: 3,
-    avatar: 'https://i.pravatar.cc/150?u=maxi_dev',
-    descipion: 'Dapelu hace e moredador aveces',
-  },
-  {
-    nick: 'dev_jony',
-    subMonths: 7,
-    avatar: 'https://i.pravatar.cc/150?u=dev_jony',
-  }
-]
+// const INICIAL_STATE = [
+//   {
+//     nick: 'maxi_dev',
+//     subMonths: 3,
+//     avatar: 'https://i.pravatar.cc/150?u=maxi_dev',
+//     descipion: 'Dapelu hace e moredador aveces',
+//   },
+//   {
+//     nick: 'dev_jony',
+//     subMonths: 7,
+//     avatar: 'https://i.pravatar.cc/150?u=dev_jony',
+//   }
+// ]
 
 interface AppState {
   subs: Sub[]
+  newSubNumber: number
 }
 
 function App() {
   const [subs, setSubs] = useState<AppState["subs"]>([]);
+  const [newSubNumber, setNewSubNumber] = useState<AppState['newSubNumber']>(0)
+  const divRef = useRef<HTMLDivElement>(null);
 
   useEffect(()=>{
-    setSubs(INICIAL_STATE)
+    // setSubs(INICIAL_STATE)
   },[])
 
   const handleNewSub = (newSub: Sub): void => {
     setSubs(subs => [...subs, newSub])
+    setNewSubNumber(n => n+1);
   }
 
   return (
-    <div className='App'>
+    <div className='App' ref={divRef} >
       <h1>Subs</h1>
       <List subs={subs} />
       <Forms onNewSub={handleNewSub} />
